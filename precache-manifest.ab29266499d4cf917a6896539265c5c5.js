f3b5deffd4be5fc5cd939e0944b8e148c6c97caf
DNZ\yvo <yvo@denieuwezaak.nl>
2019-08-27 15:35:48 +0200

    Updates

---
+++ precache-manifest.ab29266499d4cf917a6896539265c5c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea33428e835a252948ec88b6b0570c5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3b3ce4f373ebe3ba03a2",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "21cc60216f89c8100b24",
+    "url": "/robofriends/static/css/main.1764d6ea.chunk.css"
+  },
+  {
+    "revision": "3b3ce4f373ebe3ba03a2",
+    "url": "/robofriends/static/js/2.fe0f5a9f.chunk.js"
+  },
+  {
+    "revision": "21cc60216f89c8100b24",
+    "url": "/robofriends/static/js/main.40827010.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
